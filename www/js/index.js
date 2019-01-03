15b2b3ba82c4839dc11b125c722c1eba470de17c
wap4444 <wap4444@mail.ru>
2019-01-03 14:42:15 +0600

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -43,32 +43,13 @@ $('.loader1').fadeIn();
 
 
 $("#BtnEnt" ).click(function() {
-var ref = cordova.InAppBrowser.open('http://smartaksu.kz/pavlodar/smart/?push='+localStorage.ipush, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
+var ref = cordova.InAppBrowser.open('http://smartaksu.kz/pavlodar/smart/'.ipush, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
 });
         
-function didReceiveRemoteNotificationCallBack(jsonData) {}
-function didOpenRemoteNotificationCallBack(jsonData) {}       
-        //Настройка ПУШЕЙ ДЛЯ АЙФОНА
-        var iosSettings = {};
-        iosSettings["kOSSettingsKeyAutoPrompt"] = true;
-        iosSettings["kOSSettingsKeyInAppLaunchURL"] = true;
 
-        //ПОДКЛЮЧЕНИЕ ПУШЕЙ 
-           window.plugins.OneSignal
-          .startInit("0d927078-f370-4968-95d4-0eebee583306")
-          .handleNotificationReceived(didReceiveRemoteNotificationCallBack)
-          .handleNotificationOpened(didOpenRemoteNotificationCallBack)
-		  .inFocusDisplaying(window.plugins.OneSignal.OSInFocusDisplayOption.None)
-          .iOSSettings(iosSettings)
-          .endInit();
         
-window.plugins.OneSignal.getIds(function(ids) {
-ipush = ids.userId;
-$('.loader1').hide();
-localStorage.ipush=ipush;
-var ref = cordova.InAppBrowser.open('http://smartaksu.kz/pavlodar/smart/?push='+ipush, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
-$('.loader2').show();
-});
+var ref = cordova.InAppBrowser.open('http://smartaksu.kz/pavlodar/smart/', '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
+
         
 
     }
